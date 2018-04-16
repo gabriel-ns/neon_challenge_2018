2a6067dd82dd544e98f4a272d344f31d10627910
Gabriel Nascimento <gaabrielns@gmail.com>
2018-04-15 22:21:29 -0300

    created the lock controller using servo lib

---
+++ src/lib/lock_controller/lock_controller.c
@@ -0,0 +1,28 @@
+/*
+ * lock_controller.c
+ *
+ *  Created on: Apr 15, 2018
+ *      Author: gabriel
+ */
+
+#include "app_config.h"
+
+#include "servo.h"
+#include "lock_controller.h"
+
+void lock_controller_init()
+{
+	servo_init(LOCK_DEFAULT_PIN);
+	lock_controller_lock();
+
+}
+
+void lock_controller_lock()
+{
+	servo_set_angle(0);
+}
+
+void lock_controller_unlock()
+{
+	servo_set_angle(180);
+}
---
+++ src/lib/lock_controller/lock_controller.h
@@ -0,0 +1,17 @@
+/*
+ * lock_controller.h
+ *
+ *  Created on: Apr 15, 2018
+ *      Author: gabriel
+ */
+
+#ifndef SRC_LIB_LOCK_CONTROLLER_LOCK_CONTROLLER_H_
+#define SRC_LIB_LOCK_CONTROLLER_LOCK_CONTROLLER_H_
+
+void lock_controller_init();
+
+void lock_controller_lock();
+
+void lock_controller_unlock();
+
+#endif /* SRC_LIB_LOCK_CONTROLLER_LOCK_CONTROLLER_H_ */
